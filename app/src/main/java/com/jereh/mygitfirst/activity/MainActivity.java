00b8a5d7923fa628f4d6b500d91824b74dffc78c
GinoGitHub <303605997@qq.com>
2016-12-27 15:55:11 +0800

    Initial Commit

---
+++ app/src/main/java/com/jereh/mygitfirst/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jereh.mygitfirst.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jereh.mygitfirst.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
